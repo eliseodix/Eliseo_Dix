b827b82ccccf5daac7791c81457cfab97601c45e
eliseodix <eliseo.garciadix@unicolombo.edu.co>
2025-02-18 22:05:03 -0500

    Add files via upload

---
+++ taller22/src/taller22/cuentaBancaria.java
@@ -0,0 +1,21 @@
+
+package taller22;
+
+
+public class cuentaBancaria {
+    
+    int numeroCuenta;
+    int saldo;
+    String tipoCuenta;
+    public cuentaBancaria(){
+        
+    }
+    public cuentaBancaria(int numeroCuenta, String tipoCuenta){
+         this.numeroCuenta= numeroCuenta;
+         this.tipoCuenta=tipoCuenta;
+        
+    }
+    public cuentaBancaria(int numeroCuenta, int saldo, String tipoCuenta){
+        
+    }    
+}
---
+++ taller3-/src/taller3/Estudiante.java
@@ -0,0 +1,29 @@
+
+package taller3;
+
+public class Estudiante {
+    
+    String nombre;
+    int edad;
+    String curso;    
+    
+    public Estudiante(){
+        this.nombre="Desconocido";
+        this.edad= 0;
+        this.curso="Sin asignar";
+        
+    }
+    public Estudiante(String nombre, int edad){
+        this.nombre= nombre;
+        this.edad=edad;
+        this.curso="Sin asignar";
+   
+    }
+    public Estudiante(String nombre, int edad, String curso){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.nombre=nombre;
+        
+        
+    }
+}
